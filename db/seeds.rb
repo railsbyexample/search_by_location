4b86dffd251bd1e3653de43ec26589c2097ef91e
SANTIAGO PEREZ <perezperret@gmail.com>
2017-07-19 19:53:43 -0400

    add seed script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require_relative './seeds/goog_places'
+
+5.times do
+  Seller.create(
+    name: Faker::Lorem.sentence.truncate(50),
+    description: Faker::Lorem.paragraph.truncate(500)
+  )
+end
+
+Seller.all.each do |seller|
+  rand(1..5).times do
+    Store.create(
+      seller: seller,
+      name: Faker::Lorem.sentence.truncate(50),
+      description: Faker::Lorem.paragraph.truncate(500),
+      geo_location: GeoLocation.create(GOOG_PLACES.sample)
+    )
+  end
+end
+
+Store.all.each do |store|
+  rand(1..5).times do
+    Item.create(
+      store: store,
+      name: Faker::Lorem.sentence.truncate(50),
+      description: Faker::Lorem.paragraph.truncate(500)
+    )
+  end
+end
---
+++ db/seeds/goog_places.rb
@@ -0,0 +1,62 @@
+GOOG_PLACES = [
+  {
+    latitude: 40.7127837,
+    longitude: -74.00594130000002,
+    label: 'New York, NY, United States',
+    google_place_id: 'ChIJOwg_06VPwokRYv534QaPC8g'
+  },
+  {
+    latitude: 41.8781136,
+    longitude: -87.62979819999998,
+    label: 'Chicago, IL, United States',
+    google_place_id: 'ChIJ7cv00DwsDogRAMDACa2m4K8'
+  },
+  {
+    latitude: 34.0522342,
+    longitude: -118.2436849,
+    label: 'Los Angeles, CA, United States',
+    google_place_id: 'ChIJE9on3F3HwoAR9AhGJW_fL-I'
+  },
+  {
+    latitude: 37.7749295,
+    longitude: -122.41941550000001,
+    label: 'San Francisco, CA, United States',
+    google_place_id: 'ChIJIQBpAG2ahYAR_6128GcTUEo'
+  },
+  {
+    latitude: 42.3600825,
+    longitude: -71.05888010000001,
+    label: 'Boston, MA, United States',
+    google_place_id: 'ChIJGzE9DS1l44kRoOhiASS_fHg'
+  },
+  {
+    latitude: 39.9525839,
+    longitude: -75.16522150000003,
+    label: 'Philadelphia, PA, United States',
+    google_place_id: 'ChIJ60u11Ni3xokRwVg-jNgU9Yk'
+  },
+  {
+    latitude: 25.7616798,
+    longitude: -80.19179020000001,
+    label: 'Miami, FL, United States',
+    google_place_id: 'ChIJEcHIDqKw2YgRZU-t3XHylv8'
+  },
+  {
+    latitude: 26.1224386,
+    longitude: -80.13731740000003,
+    label: 'Fort Lauderdale, FL, United States',
+    google_place_id: 'ChIJ9cr6ICcB2YgRvo6_QHW-AnY'
+  },
+  {
+    latitude: 29.7604267,
+    longitude: -95.3698028,
+    label: 'Houston, TX, United States',
+    google_place_id: 'ChIJAYWNSLS4QIYROwVl894CDco'
+  },
+  {
+    latitude: 30.267153,
+    longitude: -97.74306079999997,
+    label: 'Austin, TX, United States',
+    google_place_id: 'ChIJLwPMoJm1RIYRetVp1EtGm10'
+  }
+].freeze
