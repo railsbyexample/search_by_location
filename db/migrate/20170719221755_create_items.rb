65f9aac6c9fc107f4f602ba55c03c6be77f968a0
SANTIAGO PEREZ <perezperret@gmail.com>
2017-07-19 18:18:24 -0400

    generate item model

---
+++ db/migrate/20170719221755_create_items.rb
@@ -0,0 +1,11 @@
+class CreateItems < ActiveRecord::Migration[5.1]
+  def change
+    create_table :items do |t|
+      t.string :name, limit: 50
+      t.text :description, limit: 500
+      t.references :store, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
