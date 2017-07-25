e13a6944ea3f8725d107c683f10cef0fb4e3acb5
SANTIAGO PEREZ <perezperret@gmail.com>
2017-07-25 13:35:41 -0400

    add uniqueness validation to geo_locations

--- app/models/geo_location.rb
+++ app/models/geo_location.rb
@@ -5,7 +5,7 @@ class GeoLocation < ApplicationRecord
   validates :label, presence: true
   validates :latitude, presence: true
   validates :longitude, presence: true
-  validates :google_place_id, presence: true
+  validates :google_place_id, presence: true, uniqueness: true
 
   reverse_geocoded_by :latitude, :longitude
 
--- spec/factories/geo_locations.rb
+++ spec/factories/geo_locations.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
     latitude 1.5
     longitude 1.5
     label 'MyString'
-    google_place_id 'MyString'
+    google_place_id { SecureRandom.uuid }
 
     trait :new_york do
       label 'New York'
