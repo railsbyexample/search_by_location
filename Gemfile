95b6a64dd0122d1af329d074be904aaf612a599d
Santiago Perez Perret <perezperret@gmail.com>
2018-08-19 15:41:07 -0400

    use helper for forms

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
 git_source(:github) do |repo_name|
-  repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
+  repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?('/')
   "https://github.com/#{repo_name}.git"
 end
 
@@ -37,8 +37,6 @@ group :development, :test do
   gem 'rails-controller-testing'
   # Use FactoryGirl to replace fixtures with factories
   gem 'factory_girl_rails', '~> 4.0'
-  # Use Faker for factory values
-  gem 'faker', '~> 1.7'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,31 @@
 require 'faker'
 require_relative './seeds/goog_places'
 
-5.times do
-  Seller.create(
-    name: Faker::Lorem.sentence.truncate(50),
+20.times do
+  Seller.create!(
+    name: Faker::Company.name,
     description: Faker::Lorem.paragraph.truncate(500)
   )
 end
 
+GOOG_PLACES.each { |goog_place| GeoLocation.create!(goog_place) }
+
 Seller.all.each do |seller|
   rand(1..5).times do
-    Store.create(
+    Store.create!(
       seller: seller,
-      name: Faker::Lorem.sentence.truncate(50),
+      name: Faker::Address.street_name,
       description: Faker::Lorem.paragraph.truncate(500),
-      geo_location: GeoLocation.create(GOOG_PLACES.sample)
+      geo_location: GeoLocation.order('RANDOM()').first
     )
   end
 end
 
 Store.all.each do |store|
   rand(1..5).times do
-    Item.create(
+    Item.create!(
       store: store,
-      name: Faker::Lorem.sentence.truncate(50),
+      name: "#{Faker::Appliance.brand} #{Faker::Appliance.equipment}",
       description: Faker::Lorem.paragraph.truncate(500)
     )
   end
