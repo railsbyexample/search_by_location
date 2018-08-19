8e3c2d2e091fd3d3be9545382ba1eb5c2518d36c
Santiago Perez Perret <perezperret@gmail.com>
2018-08-19 16:27:01 -0400

    fix seller n+1 query

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all.includes(store: :geo_location)
+    @items = Item.all.includes(store: [:geo_location, :seller])
 
     return if params[:geo_location_id].blank? || params[:distance].blank?
     @items = @items.near(GeoLocation.find(params[:geo_location_id]), params[:distance])
